150d0766b23cd5137d4be3d8e411f7d4443920ee
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-21 15:08:17 +0200

    initialized the project

---
+++ src/test/java/com/device_manager/WeeklyProject2U2JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.device_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyProject2U2JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
